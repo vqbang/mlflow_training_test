4c201a512e3941670638e247b5c32e4d688bd862
bang.vo <49437404+vqbang@users.noreply.github.com>
2020-08-07 16:01:16 +0700

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 logging.info(files)
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '~/key.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
 with open('key.json', 'r') as f:
     data = f.readlines()
     logging.info(data)
