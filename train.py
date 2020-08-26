cf7f0d099200f35ad503b6a1eb609fc737594018
bang.vo <49437404+vqbang@users.noreply.github.com>
2020-08-26 11:50:16 +0700

    Update train.py

--- train.py
+++ train.py
@@ -5,14 +5,14 @@
 import os
 import json
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-key_path = os.path.join(current_dir, 'key.json')
-with open(key_path, 'r') as f:
-    key = json.loads(f.read(), strict=False)
-with open(key_path, 'w') as f:
-    json.dump(key, f)
+# current_dir = os.path.abspath(os.path.dirname(__file__))
+# key_path = os.path.join(current_dir, 'key.json')
+# with open(key_path, 'r') as f:
+#     key = json.loads(f.read(), strict=False)
+# with open(key_path, 'w') as f:
+#     json.dump(key, f)
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_path
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_path
 
 # from google.cloud import storage
 # client = storage.Client()
