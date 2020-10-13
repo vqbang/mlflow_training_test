1511e400201943677f9e630fe1562b5912dbbfd4
bang.vo <49437404+vqbang@users.noreply.github.com>
2020-10-13 23:25:32 +0700

    Update train.py

--- train.py
+++ train.py
@@ -21,6 +21,7 @@ import warnings
 import sys
 import argparse
 import numpy as np
+import pandas as pd
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
