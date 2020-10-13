877d521735a4f3e14d158a7286bfc97c1673d70c
bang.vo <49437404+vqbang@users.noreply.github.com>
2020-10-13 18:28:51 +0700

    Update train.py

--- train.py
+++ train.py
@@ -9,8 +9,6 @@ import warnings
 import sys
 import argparse
 
-import pandas as pd
-import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
